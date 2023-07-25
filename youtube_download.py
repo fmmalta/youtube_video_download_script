0212637abb4d291031c9bc95d65fd21f85c89172
fmmalta <fellipedmmalta@gmail.com>
2023-07-25 00:25:32 -0300

    Updated file to save to user's desktop

--- youtube_download.py
+++ youtube_download.py
@@ -1,9 +1,11 @@
 from pytube import YouTube
 from sys import argv
+import os
 
 link = argv[1]
 yt = YouTube(link)
+desktop = os.path.join(os.path.join(os.path.expanduser("~")), "Desktop")
 
 yd = yt.streams.get_highest_resolution()
 if yd is not None:
-    yd.download("/Users/malta/Desktop")
+    yd.download(desktop)
